3f9efd38528debc4841842ca5c48d34188e38c5c
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-05-18 00:13:24 +0900

    Solve abc121_c

--- abc121/c.cpp
+++ abc121/c.cpp
@@ -4,8 +4,27 @@
 using namespace std;
 int main()
 {
-  int N;
-  cin >> N;
-  cout << N;
+  int M, N;
+  cin >> N >> M;
+  vector<pair<long long, long long>> d(N);
+  for(int i=0; i<N; ++i) cin >> d[i].first >> d[i].second;
+  sort(d.begin(), d.end());
+
+  long long cost=0;
+  int amount=0;
+  for(auto drink:d)
+  {
+    if(drink.second <= M)
+    {
+      M -= drink.second;
+      cost += drink.first*drink.second;
+    }
+    else
+    {
+      cost += drink.first*M;
+      break;
+    }
+  }
+  cout << cost << endl;
   return 0;
 }
