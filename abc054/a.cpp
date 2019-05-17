b4fd3759f31dcaefabc8d9234daa49919a42f50c
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-05-17 20:12:35 +0900

    Add abc054

---
+++ abc054/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int A, B;
+  cin >> A >> B;
+  if(A==B)
+    cout << "Draw";
+  else if(A==1 || A>B)
+    cout << "Alice";
+  else
+    cout << "Bob";
+  cout << endl;
+  return 0;
+}
