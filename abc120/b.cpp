0184b41bb232da816848bfd1184515f98960966c
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-05-18 00:19:33 +0900

    Solve abc120_b

--- abc120/b.cpp
+++ abc120/b.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 int main()
 {
-  int N;
-  cin >> N;
-  cout << N;
+  int A, B, K;
+  cin >> A >> B >> K;
+
+  int k=0;
+  for(int i=min(A,B); i>=1; --i)
+  {
+    if(A%i==0 && B%i==0)
+    {
+      ++k;
+      if(k==K)
+      {
+        cout << i << endl;
+        break;
+      }
+    }
+  }
   return 0;
 }
