a65fcd79d7c5cb2b4530b9809dcbd4e50ef4b533
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-05-17 23:16:55 +0900

    Solve abc121_b

--- abc121/b.cpp
+++ abc121/b.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
+
 int main()
 {
-  int N;
-  cin >> N;
-  cout << N;
+  int N, M, C, ans=0;
+  int B[20], A[20][20];
+  cin >> N >> M >> C;
+  for(int i=0; i<M; ++i) cin>>B[i];
+  for(int j=0; j<N; ++j) for(int i=0; i<M; ++i) cin>>A[j][i];
+  for(int j=0; j<N; ++j) {
+    int sum=0;
+    for(int i=0; i<M; ++i)
+      sum+=A[j][i]*B[i];
+    if(sum+C>0) ++ans;
+  }
+  cout << ans << endl;
   return 0;
 }
