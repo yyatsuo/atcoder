c7466ec309b25c71e46390fdc4b60a78d4f0e9f0
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-05-20 20:36:46 +0900

    Solve abc118_C

--- abc118/c.cpp
+++ abc118/c.cpp
@@ -2,10 +2,19 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int gcd(int a, int b)
+{
+  return b ? gcd(b, a%b) : a;
+}
 int main()
 {
   int N;
   cin >> N;
-  cout << N;
+  vector<int> A(N);
+  for(int i=0; i<N; ++i) cin >> A[i];
+  int ans = 0;
+  for(int i=0; i<N; ++i)
+    ans = gcd(ans,A[i]);
+  cout << ans << endl;
   return 0;
 }
