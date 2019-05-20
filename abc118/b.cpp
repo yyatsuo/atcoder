cef4ed53b66fc20ef370c89d9e198b75d8f9daa8
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-05-20 20:03:44 +0900

    Solve 118_B

--- abc118/b.cpp
+++ abc118/b.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
+static int ans_arr[30] = {0};
 int main()
 {
-  int N;
-  cin >> N;
-  cout << N;
+  int N, M;
+  cin >> N >> M;
+  for(int n=0; n<N; ++n)
+  {
+    int K;
+    cin >> K;
+    for(int i=0; i<K; ++i)
+    {
+      int A;
+      cin >> A;
+      ++ans_arr[A];
+    }
+  }
+  int ans = 0;
+  for(int i=1; i<=M; ++i)
+  {
+    if(ans_arr[i] == N) ++ans;
+  }
+  cout << ans << endl;
   return 0;
 }
