2f1336a8dd6ee6a3484698aaaf94c9a9b2bdbbbe
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-05-20 23:32:13 +0900

    Solve abc120_C

--- abc120/c.cpp
+++ abc120/c.cpp
@@ -4,8 +4,14 @@
 using namespace std;
 int main()
 {
-  int N;
-  cin >> N;
-  cout << N;
+  string S;
+  cin >> S;
+  vector<int> ans(2, 0);
+  for(char c:S)
+  {
+    if(c-'0') ++ans[0];
+    else ++ans[1];
+  }
+  cout << 2*min(ans[0],ans[1]) << endl;
   return 0;
 }
