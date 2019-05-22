f2020fce886b0f59378b2b95aee3cb57a3b2baaf
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-05-23 00:57:21 +0900

    Solve abc109_C

--- abc109/c.cpp
+++ abc109/c.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
+int gcd(int a, int b) { return b?gcd(b, a%b):a; }
+
 int main()
 {
-  int N;
-  cin >> N;
-  cout << N;
+  int N, X, x, ans=0;
+  cin >> N >> X;
+  for(int n=0; n<N; ++n)
+  {
+    cin >> x;
+    x = X-x;
+    ans = gcd(ans, x);
+  }
+  cout << abs(ans) << endl;
   return 0;
 }
