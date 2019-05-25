8f6d641d6fc73aebc6f5af2e53c548e83d54fce3
Yusuke YATSUO <yyatsuo@gmail.com>
2019-05-25 09:27:40 +0900

    Sovle abc052_B

--- abc052/b.cpp
+++ abc052/b.cpp
@@ -4,8 +4,21 @@
 using namespace std;
 int main()
 {
-  int N;
-  cin >> N;
-  cout << N;
+  int N, max=0, x=0;
+  string S;
+  cin >> N >> S;
+  for(char c:S)
+  {
+    if(c == 'I')
+    {
+      x++;
+      if(max < x) max = x;
+    }
+    else if(c == 'D')
+    {
+      x--;
+    }
+  }
+  cout << max << endl;
   return 0;
 }
