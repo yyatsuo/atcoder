b4a93f62452f89c8a0bc36ec01943155376fb140
Yusuke YATSUO <yyatsuo@gmail.com>
2019-05-25 09:37:48 +0900

    Add abc053_B

--- abc053/b.cpp
+++ abc053/b.cpp
@@ -4,8 +4,16 @@
 using namespace std;
 int main()
 {
-  int N;
-  cin >> N;
-  cout << N;
+  string S;
+  cin >> S;
+  long int start=-1, end=-1;
+  for(int i=0; i<S.size(); ++i)
+  {
+    if(S[i] == 'A' && start<0)
+      start = i;
+    if(S[i] == 'Z')
+      end = i;
+  }
+  cout << (start, end-start)+1 << endl;
   return 0;
 }
