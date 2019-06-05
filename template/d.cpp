1b8aa75ca08e87cb345ddfa8a312f2450eddf2cd
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-06-05 22:59:44 +0900

    Update template

--- template/d.cpp
+++ template/d.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i=0; i<n; ++i)
 using namespace std;
+using ll = long long;
+
 int main()
 {
   int N;
