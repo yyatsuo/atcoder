23abe199c0096de50f68274fc64bd5482e9ace76
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-06-18 00:38:06 +0900

    Solve abc097_b

--- abc079/b.cpp
+++ abc079/b.cpp
@@ -7,6 +7,13 @@ int main()
 {
   int N;
   cin >> N;
-  cout << N;
+  vector<ll> L(N+1);
+
+  for(int i=0; i<=N; ++i) {
+    if(i==0) L[i] = 2;
+    else if(i==1) L[i] = 1;
+    else L[i] = L[i-2]+L[i-1];
+  }
+  cout << L[N] << endl;
   return 0;
 }
