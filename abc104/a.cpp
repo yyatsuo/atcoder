20cb5a7638cc52542cc6de8337b38cac2e233870
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-06-18 00:52:00 +0900

    Add abc104

---
+++ abc104/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i=0; i<n; ++i)
+using namespace std;
+using ll = long long;
+
+int main()
+{
+  int N;
+  cin >> N;
+  cout << N;
+  return 0;
+}
