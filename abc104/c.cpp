aedf43f49d44a8937097d8f5c77cce83985451d0
Yusuke Yatsuo <yyatsuo@gmail.com>
2019-06-18 00:53:46 +0900

    Solve abc104_c

--- abc104/c.cpp
+++ abc104/c.cpp
@@ -1,12 +1,39 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for(int i=0; i<n; ++i)
-using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
 using ll = long long;
+using namespace std;
 
-int main()
-{
-  int N;
-  cin >> N;
-  cout << N;
-  return 0;
+int main() {
+  ll d, g;
+  cin >> d >> g;
+  g /= 100;
+  vector<int> p(d), c(d);
+  rep (i, d) {
+    cin >> p[i] >> c[i];
+    c[i] /= 100;
+  }
+
+  ll ans = INT_MAX;
+  rep (i, (1<<d)) {
+    ll cnt = 0, sum = 0;
+    rep (j, d) {
+      if (i & (1<<j)) {
+        cnt += p[j];
+        sum += p[j]*(j+1) + c[j];
+      }
+    }
+    for (ll j = d-1; j >= 0; j--) {
+      if (~i>>j & 1) {
+        for (ll k = 0; k < p[j] && sum < g; k++) {
+          sum += j+1;
+          cnt++;
+        }
+      }
+    }
+    if (sum >= g) {
+      ans = min(cnt, ans);
+    }
+  }
+  cout << ans << endl;
 }
+
