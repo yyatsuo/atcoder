ce074a94e8e4ded342a19dbda0ac028ad900862f
Yusuke YATSUO <yyatsuo@gmail.com>
2019-07-09 08:03:20 +0900

    Solve abc057_b

--- abc057/b.cpp
+++ abc057/b.cpp
@@ -5,8 +5,25 @@ using ll = long long;
 
 int main()
 {
-  int N;
-  cin >> N;
-  cout << N;
+  int N, M;
+  cin >> N >> M;
+  vector<int> a(N), b(N), c(M), d(M);
+  rep(i, N) cin >> a[i] >> b[i];
+  rep(i, M) cin >> c[i] >> d[i];
+
+  vector<pair<int,int>> ans(N, make_pair(INT_MAX, INT_MAX));
+  for(int i=0; i<N; ++i) {
+    for(int j=0; j<M; ++j) {
+      int distance = abs(a[i]-c[j])+abs(b[i]-d[j]);
+      if(distance < ans[i].first) {
+        ans[i].first = distance;
+        ans[i].second = j;
+      }
+      else if(distance == ans[i].first) {
+        ans[i].second = min(ans[i].second, j);
+      }
+    }
+  }
+  rep(i, N) cout << ans[i].second+1 << endl;
   return 0;
 }
