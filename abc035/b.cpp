d7588cb6efc1dbdfa7e9e10c16fffa00687488ba
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-02 00:24:00 +0900

    Solve abc035_b

--- abc035/b.cpp
+++ abc035/b.cpp
@@ -9,8 +9,25 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  string S; int T;
+  cin >> S >> T;
+  vector<int> N(5, 0);
+  for(char c:S) {
+    if(c=='L') { N[0]++; }
+    else if(c=='R') { N[1]++; }
+    else if(c=='U') { N[2]++; }
+    else if(c=='D') { N[3]++; }
+    else { N[4]++; }
+  }
+  int dist = abs(N[0]-N[1]) + abs(N[2]-N[3]);
+  if(T==1) {
+    dist += N[4];
+  } else {
+    for(int i=0; i<N[4]; ++i) {
+      if( dist > 0) --dist;
+      else ++dist;
+    }
+  }
+  cout << dist << endl;
 }
 
