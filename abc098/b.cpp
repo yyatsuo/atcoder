b9ab458d8d992063c902bc2be972db0975336bbf
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-01 22:30:22 +0900

    Add abc098

---
+++ abc098/b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i=0; i<n; ++i)
+using namespace std;
+using ll = long long;
+
+int main()
+{
+  int N; cin >> N;
+  string S; cin >> S;
+  int ans = 0;
+  for(int i=0; i>N; ++i) {
+  }
+  cout << ans << endl;
+}
