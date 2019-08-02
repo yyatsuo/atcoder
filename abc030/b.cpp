c1b2637bef86c98bba3cf69d4851b475b12d07cc
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-03 00:28:51 +0900

    Solve abc030_b

---
+++ abc030/b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define INF LLONG_MAX
+#define ll  long long
+#define ull unsigned long long
+#define rep(i,n) for(int i=0; i<n; ++i)
+#define MOD 1000000007
+using namespace std;
+template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
+
+int main() {
+  int h, m; cin >> h >> m;
+  float deg = abs((30*(h%12)+m*0.5)-m*6);
+  if(deg>180) cout << 360-deg << endl;
+  else cout << deg << endl;
+}
+
