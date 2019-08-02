64cf13af48bdec4a41a7a40b46b1380034707eef
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-02 23:51:20 +0900

    Solve abc032_b

--- abc032/b.cpp
+++ abc032/b.cpp
@@ -9,8 +9,15 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  string s; cin >> s;
+  int k; cin >> k;
+  vector<string> mem;
+  for(int i=0; i<(int)s.size()-k; ++i) {
+    string tmp = s.substr(i, k);
+    if(count(mem.begin(), mem.end(), tmp) == 0) {
+      mem.push_back(tmp);
+    }
+  }
+  cout << mem.size() << endl;
 }
 
