7f33be59bcc46f3b1dd9790dff4fa946a2319c9a
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-04 12:33:18 +0900

    Solve abc029_b

--- abc029/b.cpp
+++ abc029/b.cpp
@@ -9,8 +9,11 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  string S; int ans = 0;
+  rep(i, 12) {
+    cin >> S;
+    if(count(S.begin(), S.end(), 'r')) ++ans;
+  }
+  cout << ans << endl;
 }
 
