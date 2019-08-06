091a794c38c2d8a18911776de3257df37d90a6d6
yyatsuo <yyatsuo@gmail.com>
2019-08-06 23:48:14 +0900

    Solve abc129_c

--- abc129/c.cpp
+++ abc129/c.cpp
@@ -1,12 +1,27 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for(int i=0; i<n; ++i)
+#define INF LLONG_MAX
+#define ll  long long
+#define ull unsigned long long
+#define rep(i,n) for(int i=0; i<n; ++i)
+#define MOD 1000000007
 using namespace std;
-using ll = long long;
+template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
-int main()
-{
-  int N;
-  cin >> N;
-  cout << N;
-  return 0;
+int main() {
+  int N,M; cin >> N >> M;
+  vector<ll> mem(N+10,0);
+  rep(i, M) {
+    int A; cin >> A;
+    mem[A] = -1;
+  }
+  mem[0] = 1;
+  rep(i, N+1) {
+    if(mem[i] >=0 ) {
+      if(mem[i+1] >= 0) mem[i+1] = (mem[i]+mem[i+1])%MOD;
+      if(mem[i+2] >= 0) mem[i+2] = (mem[i]+mem[i+2])%MOD;
+    }
+  }
+  cout << mem[N] << endl;
 }
+
