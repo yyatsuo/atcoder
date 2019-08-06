8cb7825272656eafe8411d0ca7005b03a0c446d4
yyatsuo <yyatsuo@gmail.com>
2019-08-07 00:05:46 +0900

    Solve abc001_b

--- abc001/b.cpp
+++ abc001/b.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
-#define INF LLONG_MAX
-#define ll  long long
-#define ull unsigned long long
-#define rep(i,n) for(int i=0; i<n; ++i)
-#define MOD 1000000007
 using namespace std;
-template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
-template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  int m, vv; cin>>m;
+  if(m < 100) vv=0;
+  else if(m<=5000) vv=m/100;
+  else if(m<=30000) vv=m/1000+50;
+  else if(m<=70000) vv=(m/1000-30)/5+80;
+  else vv=89;
+  printf("%02d\n",vv);
+  return 0;
 }
 
