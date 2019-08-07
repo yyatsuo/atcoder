f248bc714f2d5c3a3ca2ab3ccb73e5002ce24475
yyatsuo <yyatsuo@gmail.com>
2019-08-07 21:10:47 +0900

    Solve abc066_b

--- abc066/b.cpp
+++ abc066/b.cpp
@@ -1,16 +1,23 @@
 #include <bits/stdc++.h>
-#define INF LLONG_MAX
-#define ll  long long
-#define ull unsigned long long
-#define rep(i,n) for(int i=0; i<n; ++i)
-#define MOD 1000000007
 using namespace std;
-template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
-template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  string s; cin >> s;
+  int size = s.size();
+  bool cont = true;
+  while(cont) {
+    --size;
+    if(size%2) --size;
+    for(int i=0; i<size/2; ++i) {
+      if(s[i] == s[size/2+i]) {
+        cont = false;
+      } else {
+        cont = true;
+        break;
+      }
+    }
+  }
+  cout << size << endl;
+  return 0;
 }
 
