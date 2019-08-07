4bdf3b0951a2b21acfa7db18b39d2fda0ff57914
yyatsuo <yyatsuo@gmail.com>
2019-08-07 21:08:25 +0900

    Solve abc001_c

--- abc001/c.cpp
+++ abc001/c.cpp
@@ -9,8 +9,30 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  int deg; float dis; cin >> deg >> dis;
+  string m; int n;
+  string dir[] = {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W","WNW", "NW", "NNW"};
+  deg = ((deg * 10 + 1125) / 2250) % 16;
+
+  dis /= 60;
+  int x = round(dis*10);
+  if(x <= 2) n = 0;
+  else if(x <= 15) n = 1;
+  else if(x <= 33) n = 2;
+  else if(x <= 54) n = 3;
+  else if(x <= 79) n = 4;
+  else if(x <= 107) n = 5;
+  else if(x <= 138) n = 6;
+  else if(x <= 171) n = 7;
+  else if(x <= 207) n = 8;
+  else if(x <= 244) n = 9;
+  else if(x <= 284) n = 10;
+  else if(x <= 326) n = 11;
+  else n = 12;
+
+  if(n == 0) m = "C";
+  else m = dir[deg];
+
+  printf("%s %d\n", m.c_str(), n);
 }
 
