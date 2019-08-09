e31d5089785ba19401148ddcb447d2376cab8d28
yyatsuo <yyatsuo@gmail.com>
2019-08-09 20:36:46 +0900

    Solve keyence2019_a

--- keyence2019/a.cpp
+++ keyence2019/a.cpp
@@ -9,8 +9,18 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  vector<int> N(4); rep(i,4) cin >> N[i];
+  vector<bool> mem(4, false);
+  for(int i:N) {
+    if(i==1) mem[0] = true;
+    else if(i==9) mem[1] = true;
+    else if(i==4) mem[2] = true;
+    else if(i==7) mem[3] = true;
+  }
+  bool flg = true;
+  for(bool b:mem) {
+    if(!b) flg = false;
+  }
+  printf(flg?"YES":"NO");
 }
 
