0f2b7c0e941cfd658a6aecafff371109f3dbcecd
yyatsuo <yyatsuo@gmail.com>
2019-08-10 22:05:24 +0900

    Solve abc055_c

--- abc055/c.cpp
+++ abc055/c.cpp
@@ -9,8 +9,16 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  ull s, c; cin >> s >> c;
+  ull ans = 0;
+  if(2*s <= c) {
+    ans += s;
+    c -= 2*s; s = 0;
+  } else {
+    ans += c/2;
+    c = c%2; s = 0;
+  }
+  ans += c/4;
+  cout << ans << endl;
 }
 
