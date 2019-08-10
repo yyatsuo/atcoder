f0a39960b885fad06e017f4dfb3d15adbdd0c10b
yyatsuo <yyatsuo@gmail.com>
2019-08-10 21:31:35 +0900

    Solve abc055_b

--- abc055/b.cpp
+++ abc055/b.cpp
@@ -1,16 +1,16 @@
-#include <bits/stdc++.h>
-#define INF LLONG_MAX
-#define ll  long long
-#define ull unsigned long long
-#define rep(i,n) for(int i=0; i<n; ++i)
-#define MOD 1000000007
+#include <iostream>
 using namespace std;
-template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
-template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
-int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+int main()
+{
+  int N;
+  cin >> N;
+  long long int ans = 1;
+  for(int i=1; i<=N; ++i) {
+    ans = ans*i;
+    ans = ans%1000000007;
+  }
+  cout << ans << endl;
+  return 0;
 }
 
