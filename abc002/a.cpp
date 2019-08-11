14755df07f46700cd9db198a90d2fc2b4356cb90
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-11 22:52:11 +0900

    Solve abc002_a

--- abc002/a.cpp
+++ abc002/a.cpp
@@ -1,16 +1,11 @@
-#include <bits/stdc++.h>
-#define INF LLONG_MAX
-#define ll  long long
-#define ull unsigned long long
-#define rep(i,n) for(int i=0; i<n; ++i)
-#define MOD 1000000007
+#include <iostream>
 using namespace std;
-template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
-template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
-int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+int main()
+{
+  int X, Y;
+  cin >> X >> Y;
+  cout << max(X,Y) << endl;
+  return 0;
 }
 
