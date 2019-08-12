710c909e940d3820ef7904290a111ac19159d977
yyatsuo <yyatsuo@gmail.com>
2019-08-12 09:42:23 +0900

    Solbve abc004_b

--- abc004/b.cpp
+++ abc004/b.cpp
@@ -1,16 +1,19 @@
 #include <bits/stdc++.h>
-#define INF LLONG_MAX
-#define ll  long long
-#define ull unsigned long long
 #define rep(i,n) for(int i=0; i<n; ++i)
-#define MOD 1000000007
 using namespace std;
-template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
-template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  char c[4][4];
+  rep(i,4) {
+    rep(j,4) {
+      cin >> c[i][j];
+    }
+  }
+  for(int i=3; i>=0; i--) {
+    for(int j=3; j>=0; j--) {
+      cout << c[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
 
