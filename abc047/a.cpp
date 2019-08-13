a994f5d509736cce17c97236845c255c3ccc08f1
yyatsuo <yyatsuo@gmail.com>
2019-08-13 23:34:38 +0900

    Solve abc047_a

--- abc047/a.cpp
+++ abc047/a.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 #define INF LLONG_MAX
-#define ll  long long
-#define ull unsigned long long
+#define Int long long
 #define rep(i,n) for(int i=0; i<n; ++i)
-#define MOD 1000000007
 using namespace std;
 template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  int a[3];
+  cin >> a[0] >> a[1] >> a[2];
+  rep(i, 3) {
+    int tmp = 2*a[i] - a[0] - a[1] - a[2];
+    if(tmp == 0) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
 }
 
