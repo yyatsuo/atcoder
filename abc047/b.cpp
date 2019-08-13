23f8ac710c4c9bde687158c4c833f66ff4490588
yyatsuo <yyatsuo@gmail.com>
2019-08-13 23:35:10 +0900

    Solve abc047_b

--- abc047/b.cpp
+++ abc047/b.cpp
@@ -9,8 +9,27 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  int W,H,N; cin >> W >> H >> N;
+  vector<int> x(N), y(N), a(N);
+  rep(i,N) cin >> x[i] >> y[i] >> a[i];
+  int x0=0, x1=W, y0=0, y1=H;
+  rep(i,N) {
+    switch(a[i]) {
+      case 1:
+        if(x0 <= x[i]) x0 = x[i];
+        break;
+      case 2:
+        if(x1 >= x[i]) x1 = x[i];
+        break;
+      case 3:
+        if(y0 <= y[i]) y0 = y[i];
+        break;
+      case 4:
+        if(y1 >= y[i]) y1 = y[i];
+        break;
+    }
+  }
+  if(x1<x0 || y1<y0) cout << 0 << endl;
+  else cout << (x1-x0)*(y1-y0) << endl;
 }
 
