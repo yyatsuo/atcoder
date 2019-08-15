025788ed6811481d4f57f29b35fc5348a878fda1
yyatsuo <yyatsuo@gmail.com>
2019-08-15 14:48:23 +0900

    Solve abc097_b

--- abc097/b.cpp
+++ abc097/b.cpp
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
-#define INF LLONG_MAX
-#define ll  long long
-#define ull unsigned long long
-#define rep(i,n) for(int i=0; i<n; ++i)
-#define MOD 1000000007
+#define rep(i, n) for(int i=0; i<n; ++i)
 using namespace std;
-template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
-template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
-
 int main() {
   int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  int ans = 1;
+  for(int i=2; ; ++i) {
+    if(i*i > N) break;
+    for(int j=1; ; ++j) {
+      int tmp = pow(i,j);
+      if(tmp > N) break;
+      if( tmp > ans) ans = tmp;
+    }
+  }
+  cout << ans << endl;
+  return 0;
 }
 
