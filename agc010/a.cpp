d41a8e7ca34eeac89800bb66f7084235f0c4faba
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-16 01:15:57 +0900

    Add agc010 & solve agc010_a

---
+++ agc010/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define INF LLONG_MAX
+#define ll  long long
+#define ull unsigned long long
+#define rep(i,n) for(int i=0; i<n; ++i)
+#define MOD 1000000007
+using namespace std;
+template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
+
+int main() {
+  int N; cin >> N;
+  int odd = 0;
+  rep(i, N) {
+    int A; cin >> A;
+    if(A%2) ++odd;
+  }
+  printf("%s\n", odd%2?"NO":"YES");
+}
+
