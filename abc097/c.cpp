a27c5bbeab1444f93019b86f8b0e631db156a080
yyatsuo <yyatsuo@gmail.com>
2019-08-15 16:37:03 +0900

    Solve abc097_c

--- abc097/c.cpp
+++ abc097/c.cpp
@@ -9,8 +9,15 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  string s; int k; cin >> s >> k;
+  vector<string> arr;
+  for(int i=1; i<=s.size() && i<=k; ++i) {
+    rep(j, s.size()-i+1) {
+      arr.push_back(s.substr(j, i));
+    }
+  }
+  sort(arr.begin(), arr.end());
+  unique(arr.begin(), arr.end());
+  cout << arr[k-1] << endl;
 }
 
