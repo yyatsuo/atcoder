e83ebb07aba1d0cfbde71e5e015ba533e886c24f
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-16 20:38:03 +0900

    Solve abc074_b

--- abc074/b.cpp
+++ abc074/b.cpp
@@ -9,8 +9,12 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  int N, K; cin >> N >> K;
+  vector<int> x(N); rep(i, N) cin >> x[i];
+  int ans = 0;
+  rep(i,N) {
+    ans += min(abs(x[i]-K), x[i])*2;
+  }
+  cout << ans << endl;
 }
 
