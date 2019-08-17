04d3bfb5b0bf17ab9ad13e3f8c544427142c3b8f
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-17 13:53:50 +0900

    Solve abc117_c

--- abc117/c.cpp
+++ abc117/c.cpp
@@ -9,8 +9,19 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  int N, M; cin >> N >> M;
+  vector<int> X(M);
+  rep(i, M) cin >> X[i];
+  sort(X.begin(), X.end());
+  vector<int> gap;
+  rep(i, M-1) {
+    gap.push_back(X[i+1]-X[i]);
+  }
+  sort(gap.begin(), gap.end());
+  int ans = 0;
+  for(int i=0; gap.size()>=N && i<=gap.size()-N; ++i) {
+    ans += gap[i];
+  }
+  cout << ans << endl;
 }
 
