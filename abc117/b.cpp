a721511fa64231dd9e4ab09042fd5306e95f8f58
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-17 13:34:55 +0900

    Solve abc117_b

--- abc117/b.cpp
+++ abc117/b.cpp
@@ -1,16 +1,22 @@
-#include <bits/stdc++.h>
-#define INF LLONG_MAX
-#define ll  long long
-#define ull unsigned long long
-#define rep(i,n) for(int i=0; i<n; ++i)
-#define MOD 1000000007
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
-template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
-int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+int main()
+{
+  int N;
+  cin >> N;
+  vector<int> l(N);
+  for(int i=0; i<N; ++i) cin>>l[i];
+  int max_idx = distance(l.begin(), max_element(l.begin(), l.end()));
+  int sum=0;
+  for(int i=0; i<N; ++i)
+ {
+   if(i != max_idx)
+     sum += l[i];
+ }
+  printf("%s\n", l[max_idx]<sum?"Yes":"No");
+  return 0;
 }
 
