bb4e3674883ff346fe6116e6479e92f4bc2f7a0a
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-22 00:16:58 +0900

    Solve abc005_c

--- abc005/c.cpp
+++ abc005/c.cpp
@@ -9,8 +9,33 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  int T, N, M; vector<int> A, B;
+  cin >> T >> N; A.resize(N);
+  rep(i,N) cin >> A[i];
+  cin >> M; B.resize(M);
+  rep(i,M) cin >> B[i];
+
+  rep(i, M) {
+    bool b = false;
+    rep(j, N) {
+      if(A[j] == -1) continue;
+      if(B[i] - A[j] > T) {
+        A[i] = -1;
+        continue;
+      } else if(0 <= (B[i] - A[j]) && (B[i] - A[j]) <= T) {
+        A[j] = -1;
+        b = true;
+        break;
+      } else if(B[i] - A[j] < 0) {
+        cout << "no" << endl;
+        return 0;
+      }
+    }
+    if(!b){
+      cout << "no" << endl;
+      return 0;
+    }
+  }
+  cout << "yes" << endl;
 }
 
