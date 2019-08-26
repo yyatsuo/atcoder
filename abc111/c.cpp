d7e53d39251c06abbd979760aa219c1ecd0d8b1b
Yusuke YATSUO <yyatsuo@gmail.com>
2019-08-27 00:35:36 +0900

    Solve abc111_c

--- abc111/c.cpp
+++ abc111/c.cpp
@@ -8,9 +8,43 @@ using namespace std;
 template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
+typedef struct {
+  int idx;
+  int cnt;
+} E;
+E a_no1, a_no2, b_no1, b_no2;
+
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  int n; cin >> n;
+  vector<int> mem1(100001,0), mem2(100001, 0);
+  rep(i, n) {
+    int v; cin >> v;
+    if(i%2) ++mem1[v];
+    else ++mem2[v];
+  }
+  rep(i, 100001) {
+    if(mem1[i] > a_no1.cnt) {
+      a_no2.idx = a_no1.idx;
+      a_no2.cnt = a_no1.cnt;
+      a_no1.idx = i;
+      a_no1.cnt = mem1[i];
+    } else if(mem1[i] > a_no2.cnt) {
+      a_no2.idx = i;
+      a_no2.cnt = mem1[i];
+    }
+    if(mem2[i] > b_no1.cnt) {
+      b_no2.idx = b_no1.idx;
+      b_no2.cnt = b_no1.cnt;
+      b_no1.idx = i;
+      b_no1.cnt = mem2[i];
+    } else if(mem2[i] > b_no2.cnt) {
+      b_no2.idx - i;
+      b_no2.cnt = mem2[i];
+    }
+  }
+  if(a_no1.cnt == b_no1.cnt && a_no1.idx == b_no1.idx)
+    cout << n - a_no1.cnt - max(a_no2.cnt, b_no2.cnt) << endl;
+  else
+    cout << n - a_no1.cnt - b_no1.cnt << endl;
 }
 
