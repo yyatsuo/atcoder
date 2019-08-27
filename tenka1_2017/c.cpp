734f2da548c35e05ff09c394a5a664706999cebe
yyatsuo <yyatsuo@gmail.com>
2019-08-27 23:56:03 +0900

    Solve tenka1_2017_c (failed)

--- tenka1_2017/c.cpp
+++ tenka1_2017/c.cpp
@@ -10,7 +10,20 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 
 int main() {
   int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  if(N%2) {
+    if(N==3) {
+      printf("2 2 3\n");
+    } else {
+      for(int A=1; A<=3500; ++A) {
+        for(int B=1; B<=3500; ++B) {
+          float C = (float)(N*A*B)/(float)(4*A*B - N*B - N*A);
+          if(floor(C) == C && (int)C>0) {
+            printf("%d %d %d\n", A, B, int(C));
+            return 0;
+          }
+        }
+      }
+    }
+  } else printf("%d %d %d\n", N, N, N/2);
 }
 
