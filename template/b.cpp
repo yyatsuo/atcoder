ea475737cf478719778fa8bd7a7b2b427e59571e
yyatsuo <yyatsuo@gmail.com>
2019-08-29 23:53:23 +0900

    Update template

--- template/b.cpp
+++ template/b.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
-#define INF LLONG_MAX
+#define INF INT_MAX
 #define ll  long long
 #define ull unsigned long long
-#define rep(i,n) for(int i=0; i<n; ++i)
+#define rep(i,n) for(ll i=0; i<n; ++i)
+#define FOR(i, s, e) for(ll i=s; i<e; ++i)
 #define MOD 1000000007
 using namespace std;
 template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
 int main() {
-  int N; cin >> N;
-  vector<int> A(N);
-  rep(i, N) cin >> A[i];
+  cin.tie(0);
+  ios::sync_with_stdio(false);
 }
 
