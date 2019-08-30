e8750e2a56744a057ae40ce4bed7dcd9421fda8d
yyatsuo <yyatsuo@gmail.com>
2019-08-30 23:13:00 +0900

    Solve abc046_c

--- abc046/c.cpp
+++ abc046/c.cpp
@@ -12,5 +12,15 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int N; cin >> N;
+  vector<int> T(N), A(N);
+  rep(i, N) cin >> T[i] >> A[i];
+  ll T_tot=T[0], A_tot=A[0];
+  for(int i=1; i<N; ++i) {
+    int n = max((T_tot+T[i]-1)/T[i], (A_tot+A[i]-1)/A[i]);
+    T_tot = T[i]*n;
+    A_tot = A[i]*n;
+  }
+  cout << T_tot + A_tot << endl;
 }
 
