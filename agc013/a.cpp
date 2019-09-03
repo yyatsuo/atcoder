ca801070918d27e01d35517f61f1c88b8098460a
yyatsuo <yyatsuo@gmail.com>
2019-09-03 23:29:03 +0900

    Solve agc013_a

--- agc013/a.cpp
+++ agc013/a.cpp
@@ -12,5 +12,27 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  ll N; cin >> N;
+  vector<ll> A(N);
+  rep(i,N) cin >> A[i];
+  int dir = 0;
+  ll ans = 1;
+  rep(i,N-1) {
+    if(dir == 1) {
+      if(A[i] > A[i+1]) {
+        ++ans;
+        dir = 0;
+      }
+    } else if (dir == -1) {
+      if(A[i] < A[i+1]) {
+        ++ans;
+        dir = 0;
+      }
+    } else if (dir == 0) {
+      if(A[i] > A[i+1]) dir = -1;
+      if(A[i] < A[i+1]) dir = 1;
+    }
+  }
+  cout << ans << endl;
 }
 
