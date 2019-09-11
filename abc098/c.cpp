0949f4f33cf67e8fb6bbd0fddd0dbb5e14613143
Yusuke YATSUO <yyatsuo@gmail.com>
2019-09-11 22:35:00 +0900

    Solve abc098

--- abc098/c.cpp
+++ abc098/c.cpp
@@ -1,12 +1,26 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for(int i=0; i<n; ++i)
+#define INF INT_MAX
+#define ll  long long
+#define ull unsigned long long
+#define rep(i,n) for(ll i=0; i<n; ++i)
+#define FOR(i, s, e) for(ll i=s; i<e; ++i)
+#define MOD 1000000007
 using namespace std;
-using ll = long long;
+template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
-int main()
-{
-  int N;
-  cin >> N;
-  cout << N;
-  return 0;
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int N; cin >> N;
+  string S; cin >> S;
+  int W=0, E=0;
+  for(auto c:S) {
+    if(c == 'W') ++W;
+    else ++E;
+  }
+  cout << W << endl;
+  cout << E << endl;
+  cout << min(W,E) - 1 << endl;
 }
+
