251e7d5ee08f3f2267e35f4413228f9e238eb7ad
yyatsuo <yyatsuo@gmail.com>
2019-09-19 00:40:31 +0900

    Solve abc073_c

--- abc073/c.cpp
+++ abc073/c.cpp
@@ -12,5 +12,21 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  ll N; cin >> N;
+  map<ll, bool> mp;
+  rep(i, N) {
+    ll A; cin >> A;
+    auto it = mp.find(A);
+    if(it == mp.end()) {
+      mp.emplace(A, true);
+    } else {
+      it->second ? it->second=false : it->second=true;
+    }
+  }
+  ll ans = 0;
+  for(auto a:mp) {
+    if(a.second) ++ans;
+  }
+  cout << ans << endl;
 }
 
