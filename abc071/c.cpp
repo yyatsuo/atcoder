9388a6959c136e26b730d6116bd0a8040fef634b
Yusuke YATSUO <yyatsuo@gmail.com>
2019-09-21 21:36:33 +0900

    Solve abc071_c

--- abc071/c.cpp
+++ abc071/c.cpp
@@ -12,5 +12,18 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int N; cin >> N;
+  vector<ll> A(N);
+  rep(i, N) cin >> A[i];
+  sort(A.begin(), A.end(), greater<int>());
+  ll x=0, y=0;
+  for(ll i=0; i<N-1; ++i) {
+    if(A[i] == A[i+1]) {
+      if(x == 0) x = A[i];
+      else if(y == 0) y = A[i];
+      ++i;
+    }
+  }
+  cout << x * y << endl;
 }
 
