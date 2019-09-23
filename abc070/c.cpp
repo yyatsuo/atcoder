7e94f0fe8203c7fba7e4fe37dc1e5ce112b1ceb3
Yusuke YATSUO <yyatsuo@gmail.com>
2019-09-23 23:05:24 +0900

    Solve abc070_c

--- abc070/c.cpp
+++ abc070/c.cpp
@@ -8,9 +8,18 @@
 using namespace std;
 template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
+template<class T> inline T gcd(T a, T b) { return a%b ? gcd(b, a%b) : b; }
+template<class T> inline T lcm(T a, T b) { return a/gcd(a,b) * b; }
 
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int N; cin >> N;
+  ull ans = 1;
+  rep(i, N) {
+    ull t; cin >> t;
+    ans = lcm(ans, t);
+  }
+  cout << ans << endl;
 }
 
