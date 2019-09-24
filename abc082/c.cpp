0d02105cd3b5eb6a85fa02cb8ad5706160b7451c
Yusuke YATSUO <yyatsuo@gmail.com>
2019-09-24 23:36:03 +0900

    Solve abc082_c

--- abc082/c.cpp
+++ abc082/c.cpp
@@ -12,5 +12,21 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  ll N; cin >> N;
+  map<ll, ll> mp;
+  rep(i, N) {
+    ll tmp; cin >> tmp;
+    if(mp.find(tmp) != mp.end()) mp[tmp]++;
+    else mp[tmp] = 1;
+  }
+  ll ans = 0;
+  for(auto i:mp) {
+    if(i.first < i.second) {
+      ans += i.second-i.first;
+    } else if(i.first > i.second) {
+      ans += i.second;
+    }
+  }
+  cout << ans << endl;
 }
 
