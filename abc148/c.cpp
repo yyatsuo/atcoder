aac001cdbec3c29de52a5a24b44face88a542728
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2019-12-29 20:36:03 +0900

    Add abc148

---
+++ abc148/c.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define ll  long long
+#define ull unsigned long long
+#define rep(i,n) for(ll i=0; i<n; ++i)
+#define FOR(i, s, e) for(ll i=s; i<e; ++i)
+#define MOD 1000000007
+using namespace std;
+template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
+
+//ll lcm(ll a, ll b) { return a*b/__gcd(a,b); }
+int main() {
+  ll A, B; cin >> A >> B;
+  cout << __lcm(A,B) << endl;
+}
+
