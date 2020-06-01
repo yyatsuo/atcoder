6db41e3ea83dd437ee3c915652e77305f0d6e653
Yusuke Yatsuo <yyatsuo@gmail.com>
2020-06-01 21:33:14 +0900

    Solve abc152_b

--- abc152/b.cpp
+++ abc152/b.cpp
@@ -12,5 +12,11 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  char a, b;
+  cin >> a >> b;
+  string str_a(b-'0', a);
+  string str_b(a-'0', b);
+  if (str_a < str_b) cout << str_a;
+  else cout << str_b;
 }
 
