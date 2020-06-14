ebe82a318c633f66da571e7364264f54024ff52c
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2020-06-14 22:06:27 +0900

    Solve abc157_b

--- abc157/b.cpp
+++ abc157/b.cpp
@@ -8,9 +8,29 @@
 using namespace std;
 template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
-
+ 
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  vector<int> A(9);
+  vector<bool> B(9, false);
+  rep(i, 9) cin >> A[i];
+  int N; cin >> N;
+  rep(i, N) {
+    int b; cin >> b;
+    rep(j, 9) {
+      if(A[j] == b) B[j] = true;
+    }
+  }
+  if(
+    (B[0] && B[1] && B[2]) ||
+    (B[3] && B[4] && B[5]) ||
+    (B[6] && B[7] && B[8]) ||
+    (B[0] && B[4] && B[8]) ||
+    (B[2] && B[4] && B[6]) ||
+    (B[0] && B[3] && B[6]) ||
+    (B[1] && B[4] && B[7]) ||
+    (B[2] && B[5] && B[8]) )
+  { cout << "Yes" << endl; }
+  else { cout << "No" << endl; }
 }
-
