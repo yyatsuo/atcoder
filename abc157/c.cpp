d4b2e1b910f7a974e669029d7f1e5e398da760a5
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2020-06-14 22:04:01 +0900

    Solve abc157_c

--- abc157/c.cpp
+++ abc157/c.cpp
@@ -12,5 +12,30 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int N; cin >> N;
+  int M; cin >> M;
+  vector<int> a(N, -1);
+  rep(i, M) {
+    int s, c;
+    cin >> s >> c;
+    if(a[s-1] != -1 && a[s-1] != c) {
+      cout << -1 << endl;
+      return 0;
+    }
+    a[s-1] = c;
+  }
+  if(a[0] == 0 && N != 1) {
+    cout << -1 << endl;
+    return 0;
+  }
+  if(a[0] == -1) {
+    if(N==1) a[0] = 0;
+    else a[0] = 1;
+  }
+  for(auto i:a) {
+    if(i == -1) cout << 0;
+    else cout << i;
+  }
+  cout << endl;
 }
 
