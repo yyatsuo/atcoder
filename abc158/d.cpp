8943860b3c9928227d744c54190629fc741969c9
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2020-06-18 21:40:57 +0900

    Solve abc158_d

--- abc158/d.cpp
+++ abc158/d.cpp
@@ -12,5 +12,28 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  string S; S.reserve(300000); cin >> S;
+  ll Q; cin >> Q;
+  int dir = 1;
+  rep(i, Q) {
+    int T; cin >> T;
+    if(T == 1) {
+      dir *= -1;
+    } else {
+      int F; string c;
+      cin >> F >> c;
+      if(F == 1 && dir == 1) S.insert(0,c);
+      else if(F == 2 && dir == -1) S.insert(0,c);
+      else if(F == 1 && dir == -1) S += c;
+      else if(F == 2 && dir == 1) S += c;
+      else cerr << "Error" << endl;
+    }
+  }
+  if(dir == -1) {
+    for(ll i=S.size()-1; i>=0; --i) cout << S[i];
+  } else {
+    cout << S;
+  }
+  cout << endl;
 }
 
