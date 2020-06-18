42b2cad0ec38af4aebfaf7dc0e1a71956a0dcf64
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2020-06-18 21:53:16 +0900

    Solve abc159_a

--- abc159/a.cpp
+++ abc159/a.cpp
@@ -1,16 +1,8 @@
 #include <bits/stdc++.h>
-#define INF INT_MAX
-#define ll  long long
-#define ull unsigned long long
-#define rep(i,n) for(ll i=0; i<n; ++i)
-#define FOR(i, s, e) for(ll i=s; i<e; ++i)
-#define MOD 1000000007
 using namespace std;
-template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
-template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
-
+int f(int n) { return (n*(n-1))/2; }
 int main() {
-  cin.tie(0);
-  ios::sync_with_stdio(false);
+  int N, M; cin >> N >> M;
+  cout << f(N) + f(M) << endl;
 }
 
