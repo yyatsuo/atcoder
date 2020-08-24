c589a6ca78aa579d5fce9b9add89320ff96c44ed
Yusuke Yatsuo <yyatsuo@gmail.com>
2020-08-24 23:22:19 +0900

    Add aising2020

---
+++ aising2020/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define ll  long long
+#define ull unsigned long long
+#define rep(i,n) for(ll i=0; i<n; ++i)
+#define FOR(i, s, e) for(ll i=s; i<e; ++i)
+#define MOD 1000000007
+using namespace std;
+template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int L, R, d;
+  cin >> L >> R >> d;
+  int ans = 0;
+  for(int i=L; i<=R; ++i) {
+    if(i%d == 0) ++ans;
+  }
+  cout << ans << endl;
+}
+
---
+++ aising2020/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define ll  long long
+#define ull unsigned long long
+#define rep(i,n) for(ll i=0; i<n; ++i)
+#define FOR(i, s, e) for(ll i=s; i<e; ++i)
+#define MOD 1000000007
+using namespace std;
+template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int N; cin >> N;
+  int ans = 0;
+  rep(i, N) {
+    int tmp; cin >> tmp;
+    if(i%2==0 && tmp%2==1) ++ans;
+  }
+  cout << ans << endl;
+}
+
