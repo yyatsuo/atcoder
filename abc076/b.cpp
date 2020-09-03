7429090ecc924d717acd7f3a860027888fc0bc4d
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2020-09-03 23:00:31 +0900

    Solve abc076_B

--- abc076/b.cpp
+++ abc076/b.cpp
@@ -12,5 +12,11 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int N, K; cin >> N >> K;
+  int ans = 1;
+  rep(i, N) {
+    ans = min(ans*2, ans+K);
+  }
+  cout << ans << endl;
 }
 
