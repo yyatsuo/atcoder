49befca1d953b305d3810de2233dd54cdc06375a
Yusuke Yatsuo <yyatsuo@gmail.com>
2020-09-16 20:12:18 +0900

    Solve abc170_B

--- abc170/b.cpp
+++ abc170/b.cpp
@@ -12,5 +12,13 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int X, Y; cin>>X>>Y;
+  rep(n,X+1) {
+    if( (n*2+(X-n)*4)==Y ) {
+      cout << "Yes" <<endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
 }
 
