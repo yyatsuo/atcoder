46c9062e794dee7aebd4ad5ae635a94777322cc1
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2020-09-30 00:36:32 +0900

    Solve abc170_C

--- abc170/c.cpp
+++ abc170/c.cpp
@@ -12,5 +12,20 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int X, N;
+  cin>>X>>N;
+  vector<int> P(N);
+  rep(i,N) cin>>P[i];
+  sort(P.begin(),P.end());
+  for(int i=0;;++i) {
+    if(!(binary_search(P.begin(), P.end(), X-i))) {
+      cout << X-i << endl;
+      return 0;
+    }
+    if(!(binary_search(P.begin(), P.end(), X+i))) {
+      cout << X+i << endl;
+      return 0;
+    }
+  }
 }
 
