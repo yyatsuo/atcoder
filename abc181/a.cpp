56fb04ec147fd135bedf5e46632af85a4adbf642
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2020-12-17 00:06:24 +0900

    Solve abc181_a

--- abc181/a.cpp
+++ abc181/a.cpp
@@ -12,5 +12,7 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int N; cin >> N;
+  printf("%s\n", N%2?"Black":"White");
 }
 
