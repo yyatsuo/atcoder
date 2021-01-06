a5e9df730ac214c247fadb64e12db2494e43e91b
Yusuke Yatsuo <yyatsuo@gmail.com>
2021-01-06 22:37:31 +0900

    Solve abc187_b

--- abc187/b.cpp
+++ abc187/b.cpp
@@ -12,5 +12,17 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int N; cin >> N;
+  vector<float> x(N), y(N);
+  rep(i, N) cin >> x[i] >> y[i];
+
+  int ans = 0;
+  for(int i=0; i<N; ++i) {
+    for(int j=i+1; j<N; ++j) {
+      float a = (y[j]-y[i])/(x[j]-x[i]);
+      if(-1 <= a && a <= 1) ++ans;
+    }
+  }
+  cout << ans << endl;
 }
 
