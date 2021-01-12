91d5b2f975b4700c2a1a40bced875061b87cc875
Yusuke Yatsuo <yyatsuo@gmail.com>
2021-01-12 23:11:15 +0900

    Solve abc181_c

--- abc181/c.cpp
+++ abc181/c.cpp
@@ -12,5 +12,20 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int N; cin >> N;
+  vector<double> x(N), y(N);
+  rep(i, N) cin >> x[i] >>y[i];
+  for(int i=0; i<N; ++i) {
+    for(int j=i+1; j<N; ++j) {
+      for(int k=j+1; k<N; ++k) {
+        if((y[j]-y[i])*(x[k]-x[i]) == (x[j]-x[i])*(y[k]-y[i])) {
+          cout << "Yes" << endl;
+          return 0;
+        }
+      }
+    }
+  }
+  cout << "No" << endl;
+  return 0;
 }
 
