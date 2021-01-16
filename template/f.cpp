702108a9d166ea48873079ecaf9b8498df253daf
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2021-01-16 20:44:51 +0900

    Update template

--- template/f.cpp
+++ template/f.cpp
@@ -3,11 +3,13 @@
 #define ll  long long
 #define ull unsigned long long
 #define rep(i,n) for(ll i=0; i<n; ++i)
+#define repp(i,n) for(ll i=1; i<=n; ++i)
 #define FOR(i, s, e) for(ll i=s; i<e; ++i)
 #define MOD 1000000007
 using namespace std;
 template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
+int gcd(int x, int y) { if(x % y == 0) { return y; } else { return gcd(y, x % y); } }
 
 int main() {
   cin.tie(0);
