a4461dbc166350db4b5c90666914172bc5b5173d
Yusuke Yatsuo <yyatsuo@gmail.com>
2021-01-17 21:35:23 +0900

    Solve abc182_c

--- abc182/c.cpp
+++ abc182/c.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define INF INT_MAX
+#define ULL_MAX ULLONG_MAX
 #define ll  long long
 #define ull unsigned long long
 #define rep(i,n) for(ll i=0; i<n; ++i)
@@ -12,5 +13,30 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  string N; cin >> N;
+
+  int ans = INF;
+  bool found = false;
+  for(int bit=0; bit < (1 << N.length()); ++bit) {
+    int tmp = 0;
+    string numstr;
+    for(int i=0; i<N.length(); ++i) {
+      int mask = 1 << i;
+      if(bit & mask) {
+        ++tmp;
+      } else {
+        numstr.push_back(N[i]);
+      }
+    }
+    if(numstr.size() != 0) {
+      ull num = stoull(numstr);
+      if(num%3 == 0) {
+        chmin(ans, tmp);
+        found = true;
+      }
+    }
+  }
+  if(found) cout << ans << endl;
+  else cout << -1 << endl;
 }
 
