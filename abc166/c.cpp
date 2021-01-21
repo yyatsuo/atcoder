3399d5373372fe01054109f84a28ea8c7c35318f
Yusuke Yatsuo <yyatsuo@gmail.com>
2021-01-22 01:39:26 +0900

    Solve abc166_c

--- abc166/c.cpp
+++ abc166/c.cpp
@@ -9,13 +9,37 @@ using namespace std;
 template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 
+typedef struct{
+  vector<int> n;
+} NL;
+
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
   int N, M; cin>>N>>M;
-  vector<ll> H(N);
-  vector<int> A(M), B(M);
-  rep(i,N) cin >>H[i];
-  rep(i,M) cin>>A[i]>>B[i];
+
+  vector<ll> H(N+1);
+  for(int i=1; i<=N; ++i) cin >>H[i];
+
+  vector<NL> nl(N+1);
+
+  rep(i,M) {
+    int A, B;
+    cin >> A >> B;
+    nl[A].n.push_back(B);
+    nl[B].n.push_back(A);
+  }
+
+  int ans = 0;
+  for(int i=1; i<=N; ++i) {
+    bool isGood = true;
+    for(int n:nl[i].n) {
+      if(H[n] >= H[i]) {
+        isGood = false;
+      }
+    }
+    if(isGood) ++ans;
+  }
+  cout << ans << endl;
 }
 
