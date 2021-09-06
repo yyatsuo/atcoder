edbb842a76e458bf70642afef401eaae40676ad6
Yusuke Yatsuo <yyatsuo@gmail.com>
2021-09-06 23:59:40 +0900

    Solve abc187 d

--- abc187/d.cpp
+++ abc187/d.cpp
@@ -12,5 +12,28 @@ template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} ret
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  ll N; cin >> N;
+  ll Asum = 0;
+  vector<tuple<ll,ll,ll>> arr;
+  rep(i, N) {
+    ll A, B; cin >> A >> B;
+    ll sum = A+B;
+    ll diff = sum + A;
+    arr.push_back(make_tuple(diff, A, B));
+    Asum += A;
+  }
+  sort(arr.begin(), arr.end(), greater<tuple<ll,ll,ll>>());
+
+  ll ans = 0;
+  ll T = 0;
+  for( auto a:arr ) {
+    ++ans;
+    T += get<1>(a) + get<2>(a);
+    Asum -= get<1>(a);
+    if(Asum < T) {
+      cout << ans << endl;
+      return 0;
+    }
+  }
 }
 
