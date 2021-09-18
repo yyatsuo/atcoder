ac97f184df1a08c45f827815fca377ab44333d77
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2021-09-19 00:25:44 +0900

    Solve abc200 a b

---
+++ abc200/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define ll  long long
+#define ull unsigned long long
+#define rep(i,n) for(ll i=0; i<n; ++i)
+#define repp(i,n) for(ll i=1; i<=n; ++i)
+#define FOR(i, s, e) for(ll i=s; i<e; ++i)
+#define MOD 1000000007
+using namespace std;
+template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
+int gcd(int x, int y) { if(x % y == 0) { return y; } else { return gcd(y, x % y); } }
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  ll N; cin >> N;
+  ll K; cin >> K;
+  rep(i,K) {
+    if(N%200==0) N = N/200;
+    else N = N*1000+200;
+  }
+  cout << N << endl;
+}
