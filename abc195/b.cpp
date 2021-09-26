7a852806f1a26119f351d28024098cc4f153fbc8
Yusuke Yatsuo <yyatsuo@debian.or.jp>
2021-09-26 19:44:24 +0900

    Solve abc195 b

--- abc195/b.cpp
+++ abc195/b.cpp
@@ -4,7 +4,7 @@
 #define ull unsigned long long
 #define rep(i,n) for(ll i=0; i<n; ++i)
 #define repp(i,n) for(ll i=1; i<=n; ++i)
-#define FOR(i, s, e) for(ll i=s; i<e; ++i)
+#define FOR(i, s, e) for(int i=s; i<e; ++i)
 #define MOD 1000000007
 using namespace std;
 template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
@@ -14,5 +14,18 @@ int gcd(int x, int y) { if(x % y == 0) { return y; } else { return gcd(y, x % y)
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int A, B, W;
+  cin >> A >> B >> W;
+  W *= 1000;
+  int mi=INF, ma=-1;
+  FOR(choose, 1, W+1) {
+    int L = choose * A;
+    int R = choose * B;
+    if(L <= W && W <= R) {
+      chmin(mi, choose);
+      chmax(ma, choose);
+    }
+  }
+  if(mi==INF) cout << "UNSATISFIABLE" << endl;
+  else cout << mi << " " << ma << endl;
 }
-
