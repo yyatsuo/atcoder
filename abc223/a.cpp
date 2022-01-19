7177f2532b4b1c2953d822bf46ec7d069b4ea43f
Yusuke Yatsuo <yyatsuo@gmail.com>
2022-01-19 23:05:39 +0900

    Solve abc223 a

--- abc223/a.cpp
+++ abc223/a.cpp
@@ -8,6 +8,7 @@
 #define FOR(i, s, e) for(ll i=s; i<e; ++i)
 #define MOD 1000000007
 #define YesNo(e) printf("%s\n", e ? "Yes" : "No");
+#define NoYes(e) printf("%s\n", e ? "No" : "Yes");
 
 using namespace std;
 template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
@@ -17,5 +18,8 @@ int gcd(int x, int y) { if(x % y == 0) { return y; } else { return gcd(y, x % y)
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int X; cin >> X;
+  if(X) {NoYes(X%100);}
+  else {cout << "No" << endl;}
 }
 
