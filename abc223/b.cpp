7ba2b650eb72debabf3de9b84efdb8446b1b45c8
Yusuke Yatsuo <yyatsuo@gmail.com>
2022-01-31 23:36:16 +0900

    Solve abc223 b

--- abc223/b.cpp
+++ abc223/b.cpp
@@ -17,5 +17,15 @@ int gcd(int x, int y) { if(x % y == 0) { return y; } else { return gcd(y, x % y)
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  string S; cin >> S;
+  string Smin = S;
+  string Smax = S;
+  rep(i, S.size()) {
+    rotate(S.begin(), S.begin()+1, S.end());
+    if(S < Smin) Smin = S;
+    if(S > Smax) Smax = S;
+  }
+  cout << Smin << endl;
+  cout << Smax << endl;
 }
 
