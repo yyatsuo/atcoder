81b53f3ef7d70af37d5cde72076e78663c5a3a66
Yusuke Yatsuo <yyatsuo@gmail.com>
2022-02-03 23:18:17 +0900

    Solve abc224 b

--- abc224/b.cpp
+++ abc224/b.cpp
@@ -15,8 +15,30 @@ template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} ret
 template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
 int gcd(int x, int y) { if(x % y == 0) { return y; } else { return gcd(y, x % y); } }
 
+int A[55][55];
+
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int H, W; cin >> H >> W;
+  rep(i,H) {
+    rep(j,W) {
+      cin >> A[i][j];
+    }
+  }
+
+  for(int i1=0; i1<H-1; i1++) {
+    for(int i2=i1+1; i2<H; i2++) {
+      for(int j1=0; j1<W-1; j1++) {
+        for(int j2=j1+1; j2<W; j2++) {
+          if(A[i1][j1]+A[i2][j2] > A[i2][j1] + A[i1][j2]) {
+            cout << "No" << endl;
+            return 0;
+          }
+        }
+      }
+    }
+  }
+  cout << "Yes" << endl;
 }
 
