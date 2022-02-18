2287e14960de41eabb37f3e318ba3a6193de4af1
Yusuke Yatsuo <yyatsuo@gmail.com>
2022-02-19 00:05:53 +0900

    Solve abc236 a

---
+++ abc236/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define INF INT_MAX
+#define ll  long long
+#define ull unsigned long long
+#define rep(i,n) for(ll i=0; i<n; ++i)
+#define repp(i,n) for(ll i=1; i<=n; ++i)
+#define FOR(i, s, e) for(ll i=s; i<e; ++i)
+#define MOD 1000000007
+#define YesNo(e) printf("%s\n", e ? "Yes" : "No");
+#define NoYes(e) printf("%s\n", e ? "No" : "Yes");
+
+using namespace std;
+template<class T> inline bool chmin(T& a, T b) { if(a>b) {a=b; return true;} return false;}
+template<class T> inline bool chmax(T& a, T b) { if(a<b) {a=b; return true;} return false;}
+int gcd(int x, int y) { if(x % y == 0) { return y; } else { return gcd(y, x % y); } }
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  string S; int a, b;
+  cin >> S >> a >> b;
+  swap(S[--a],S[--b]);
+  cout << S << endl;
+}
+
