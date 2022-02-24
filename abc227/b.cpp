511f0e25e4fb6e3ebb8aac68e3948026463ce54e
Yusuke Yatsuo <yyatsuo@gmail.com>
2022-02-24 21:34:22 +0900

    Solve abc227 b

--- abc227/b.cpp
+++ abc227/b.cpp
@@ -18,5 +18,24 @@ int gcd(int x, int y) { if(x % y == 0) { return y; } else { return gcd(y, x % y)
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  int N; cin >> N;
+  vector<int> S(N);
+  rep(i,N) cin >> S[i];
+
+  vector<int> area;
+  for(int a=1; a<=250; ++a) {
+    for(int b=1; b<=250; ++b) {
+      int tmp = 4*a*b + 3*a + 3*b;
+      area.push_back(tmp);
+    }
+  }
+  sort(area.begin(), area.end());
+
+  int ans = 0;
+  for(int s:S) {
+    if( ! binary_search(area.begin(), area.end(), s) ) ++ans;
+  }
+
+  cout << ans << endl;
 }
 
