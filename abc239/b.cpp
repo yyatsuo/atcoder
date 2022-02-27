7421d2e7bed7c20950f494cd5c7e930fe1134aca
Yusuke Yatsuo <yyatsuo@gmail.com>
2022-02-27 22:12:14 +0900

    Solve abc239 b

--- abc239/b.cpp
+++ abc239/b.cpp
@@ -18,5 +18,24 @@ int gcd(int x, int y) { if(x % y == 0) { return y; } else { return gcd(y, x % y)
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
+  string S; cin >> S;
+  if(S.size() == 1)
+  {
+    cout << 0 << endl;
+  }
+  else if(S[0] != '-' || S[S.size()-1] == '0')
+  {
+    S.pop_back();
+    cout << S << endl;
+  }
+  else if(S.size()==2 && S[0]=='-')
+  {
+    cout << -1 << endl;
+  }
+  else
+  {
+    S.pop_back();
+    printf("%lld\n", stoll(S)-1 );
+  }
 }
 
